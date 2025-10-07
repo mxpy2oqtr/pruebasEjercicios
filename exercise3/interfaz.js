57ec730578539d7311e98ead238b2a5bf7a730c6
mxpy2oqtr <dulcekoko@gmail.com>
2025-10-07 16:33:57 +0100

    ADD exercise 3

---
+++ exercise3/interfaz.js
@@ -0,0 +1,8 @@
+function turnOn(){
+    document.getElementById('myImage').src='./images/white.png';
+    document.getElementById('myText').innerText=' White Bear';
+}
+function turnOff(){
+    document.getElementById('myImage').src='./images/black.png';
+    document.getElementById('myText').innerText=' Black Bear';
+}
